85f281ae0dd1c2aee68a803a268276732f51e087
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-07 19:10:44 -0400

    Update Helpers.py

--- manual_phighting_idiosyncraticerror/Helpers.py
+++ manual_phighting_idiosyncraticerror/Helpers.py
@@ -9,7 +9,7 @@ from enum import IntEnum
 from typing import Optional, List, Union, get_args, get_origin, Any
 from types import GenericAlias
 from worlds.AutoWorld import World
-from .hooks.Helpers import before_is_category_enabled, before_is_item_enabled, before_is_location_enabled, before_is_event_enabled
+from .hooks.Helpers import before_is_category_enabled, before_is_item_enabled, before_is_location_enabled#, before_is_event_enabled
 
 
 # blatantly copied from the minecraft ap world because why not
