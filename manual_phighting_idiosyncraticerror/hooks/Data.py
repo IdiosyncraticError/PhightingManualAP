29e3ff505efbd73661d314744a981d22e90281f9
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-07 19:21:40 -0400

    Update Data.py

--- manual_phighting_idiosyncraticerror/hooks/Data.py
+++ manual_phighting_idiosyncraticerror/hooks/Data.py
@@ -16,6 +16,11 @@ def after_load_progressive_item_file(progressive_item_table: list) -> list:
 def after_load_location_file(location_table: list) -> list:
     return location_table
 
+# called after the events.json file has been loaded, before any processing has occurred
+# If you need access to the events after processing, you should use the hooks in World.py
+def after_load_event_file(event_table: list) -> list:
+    return event_table
+
 # called after the locations.json file has been loaded, before any location loading or processing has occurred
 # if you need access to the locations after processing to add ids, etc., you should use the hooks in World.py
 def after_load_region_file(region_table: dict) -> dict:
