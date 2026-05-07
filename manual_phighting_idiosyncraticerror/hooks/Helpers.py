7c7a1bce711b450098561c3e08993482cbe76144
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-07 19:52:55 -0400

    Update Helpers.py

--- manual_phighting_idiosyncraticerror/hooks/Helpers.py
+++ manual_phighting_idiosyncraticerror/hooks/Helpers.py
@@ -1,9 +1,6 @@
 from typing import Optional, Any
-from BaseClasses import MultiWorld, Item, Location
+from BaseClasses import MultiWorld
 
-if TYPE_CHECKING:
-    from ..Items import ManualItem
-    from ..Locations import ManualLocation
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
@@ -12,12 +9,12 @@ def before_is_category_enabled(multiworld: MultiWorld, player: int, category_nam
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
-def before_is_item_enabled(multiworld: MultiWorld, player: int, item: "ManualItem") -> Optional[bool]:
+def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str, Any]) -> Optional[bool]:
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the location, False to disable it, or None to use the default behavior
-def before_is_location_enabled(multiworld: MultiWorld, player: int, location: "ManualLocation") -> Optional[bool]:
+def before_is_location_enabled(multiworld: MultiWorld, player: int, location:  dict[str, Any]) -> Optional[bool]:
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
