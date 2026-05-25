89820af65bb224098eaefd4803ab7fd99e2600b9
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-24 21:15:09 -0400

    item gen progress

--- items_gen.py
+++ items_gen.py
@@ -35,22 +35,31 @@ for phighter in phighters:
     output.append(obj)
     
 for phighter in phighters:
-    for ab in abilities:
-        obj = {}
-        obj["count"] = 1
-        obj["name"] = phighter + " " + ab + " Unlock"
     if phighter == "Sword":
         for ab in abilities:
+            obj = {
+                "count": 1,
+                "name": "Sword Base " + ab + " Unlock"
+            }
             pass
-    if phighter == "Skateboard":
+    elif phighter == "Skateboard":
         for ab in abilities:
             pass
-    if phighter == "Scythe":
+    elif phighter == "Scythe":
         for ab in abilities:
             pass
-    if phighter == "Coil":
+    elif phighter == "Coil":
         for ab in abilities:
             pass
+    else:
+        for ab in abilities:
+            obj = {}
+            obj["count"] = 1
+            obj["name"] = phighter + " " + ab + " Unlock"
+            obj["category"] = [phighter + " Ability Unlock"]
+            obj["progression"] = True
+            output.append(obj)
+
 
 with open("data.json", "w") as file:
     json.dump(output, file, indent=4)
