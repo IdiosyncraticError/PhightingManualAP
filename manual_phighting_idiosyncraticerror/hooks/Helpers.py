269897f4d0f24853dda88a4621898d8ed4664e83
syncro <IdiosyncraticError@users.noreply.github.com>
2026-06-09 13:33:03 -0400

    fix 1

--- manual_phighting_idiosyncraticerror/hooks/Helpers.py
+++ manual_phighting_idiosyncraticerror/hooks/Helpers.py
@@ -29,20 +29,7 @@ def before_is_category_enabled(multiworld: MultiWorld, player: int, category_nam
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
-def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str, Any]) -> Optional[bool]:
-    if item["name"].endswith("Unlock"):
-        from ..Helpers import get_option_value
-        enabled_class = get_option_value(multiworld, player, "enabled_classes")
-        return item["name"] in enabled_class
-
-    if item["name"].endswith("Win Progression Flag"):
-        from ..Helpers import get_option_value
-        enabled_class = get_option_value(multiworld, player, "enabled_classes")
-        for i in enabled_class:
-            if item["name"].startswith(i.removesuffix("Unlock")):
-                return True
-        return False
-    
+def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str, Any]) -> Optional[bool]:    
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
