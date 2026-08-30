267d43bbb784d3b8bdba79a52137fd462ba34459
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-08-30 11:33:36 -0500

    remove useless debug lmao

--- manual_phighting_idiosyncraticerror/hooks/Helpers.py
+++ manual_phighting_idiosyncraticerror/hooks/Helpers.py
@@ -17,10 +17,6 @@ def before_is_category_enabled(multiworld: MultiWorld, player: int, category_nam
         enabled_phighters = []
         for p in phighters:
             enabled_phighters.append(p + " Ability Unlock")
-        print(category_name in enabled_ab and category_name in enabled_phighters)
-        print(category_name)
-        print(enabled_ab)
-        print(enabled_phighters)
         return (category_name in enabled_ab and category_name in enabled_phighters)
     
     if category_name.endswith(" MVP Badges"):
